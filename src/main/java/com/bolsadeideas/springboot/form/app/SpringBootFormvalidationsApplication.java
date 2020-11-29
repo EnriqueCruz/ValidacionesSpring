76ed013d686bce92d54123d855ad7828c1c294f6
Enrique Cruz <enrique_magdalenocruz@hotmail.com>
2020-11-28 18:14:07 -0600

    Se crea proyecto Spring para validación de formularios

---
+++ src/main/java/com/bolsadeideas/springboot/form/app/SpringBootFormvalidationsApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.form.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFormvalidationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFormvalidationsApplication.class, args);
+	}
+
+}
