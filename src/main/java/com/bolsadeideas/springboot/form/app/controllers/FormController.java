919e2195b92b1f685b7d6db3d14418e86879e0c5
Enrique Cruz <enrique_magdalenocruz@hotmail.com>
2020-12-06 16:44:05 -0600

    chore: User entity class created
    A entity class named User has benn created for be used as entity
    object in future, also the controller was changed implementing
    the new entity class to retrieve data from the form.

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.bolsadeideas.springboot.form.app.controllers.entity.User;
+
 @Controller
 public class FormController {
 	
@@ -15,17 +17,31 @@ public class FormController {
 		return "form";
 	}
 	
+
+	
+	/* If Entity object is the first parameter in the controller´s method, then 
+	 * the form.html attributes will be mapped to the entitity´s fields
+	 * (always when the form attributes are the same than User class)*/
+	@RequestMapping(value="/sendInformation", method = RequestMethod.POST)
+	public String process(User user, Model model) {
+		model.addAttribute("title", "User information");
+		model.addAttribute("user", user);
+		return "infoUser";	
+	}
+	
+	/* Verbose way of 'process' method
 	@RequestMapping(value="/sendInformation", method = RequestMethod.POST)
 	public String procesar(Model model,
 			@RequestParam String userName,
 			@RequestParam String email,
 			@RequestParam String password) {
+		User user = new User();
+		user.setUserName(userName);
+		user.setEmail(email);
+		user.setPassword(password);
 		model.addAttribute("title", "User information");
-		model.addAttribute("userName", userName);
-		model.addAttribute("email", email);
-		model.addAttribute("password", password);
-		return "infoUser";
-		
+		model.addAttribute("user", user);
+		return "infoUser";	
 	}
-	
+	*/
 }
