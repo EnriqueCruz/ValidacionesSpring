5078aafb24ff7aff87219cb6ea27d7cfabed0294
Enrique Cruz <enrique_magdalenocruz@hotmail.com>
2020-12-06 15:57:41 -0600

    feature: login page
    A login page has been created, using a form page, then user can
    introduce his credentials and his information will be showed
    in other page.

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -4,17 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
 	
-	@RequestMapping(name="/form", method = RequestMethod.GET)
-	public String form() {
-		return "formulario";
+	@RequestMapping(value="/formUser", method = RequestMethod.GET)
+	public String form(Model model) {
+		model.addAttribute("title", "Form user");
+		return "form";
 	}
 	
-	@RequestMapping(name="/envio", method = RequestMethod.POST)
-	public void envioDatos(Model model) {
+	@RequestMapping(value="/sendInformation", method = RequestMethod.POST)
+	public String procesar(Model model,
+			@RequestParam String userName,
+			@RequestParam String email,
+			@RequestParam String password) {
+		model.addAttribute("title", "User information");
+		model.addAttribute("userName", userName);
+		model.addAttribute("email", email);
+		model.addAttribute("password", password);
+		return "infoUser";
 		
 	}
 	
